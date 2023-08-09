9a8082d9fc5bea5aadf0db3bb809e47710862317
yarden <yardenmev@gmail.com>
2023-08-09 19:11:57 +0000

    working on Terraform infa

---
+++ Terraform/provider.tf
@@ -0,0 +1,26 @@
+provider "google"{
+    project = "gcp-devops-training-389818"
+    region  = "us-central1"
+    credentials = file("key.json")
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "infa-deploy-yarden-staging"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
